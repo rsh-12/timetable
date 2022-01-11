0a9597f92ad1ac1052629572d5ba1dd5b58ee8a4
rsh-12 <grinvichforum12@mail.ru>
2022-01-11 21:50:02 +0500

    Init commit

---
+++ src/main/java/ru/timetable/TimetableApiApplication.java
@@ -0,0 +1,13 @@
+package ru.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/timetable/TimetableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
