ad5a299561c0fc1c0eb3e3ee9b56afa8052b7947
rsh-12 <grinvichforum12@mail.ru>
2022-01-12 01:01:01 +0500

    Add DatasourceConfig class

---
+++ src/main/java/ru/timetable/config/DatasourceConfig.java
@@ -0,0 +1,32 @@
+package ru.timetable.config;
+/*
+ * Date: 12.01.2022
+ * Time: 12:58 AM
+ * */
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource) {
+        return new JdbcTemplate(hikariDataSource);
+    }
+
+}
