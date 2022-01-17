364d4bc9b9c67c8d7782f9a19c56174d6ac6cc0a
rsh-12 <grinvichforum12@mail.ru>
2022-01-17 07:05:50 +0500

    Add PostgreSqlTestBase.java

---
+++ src/test/java/ru/timetable/repository/PostgreSqlTestBase.java
@@ -0,0 +1,26 @@
+package ru.timetable.repository;
+/*
+ * Date: 15.01.2022
+ * Time: 11:04 PM
+ * */
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class PostgreSqlTestBase {
+
+    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:14");
+
+    static {
+        database.start();
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+    }
+
+}
