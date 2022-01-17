9e3c55007d87dba7566fa0e7dcf5a24cc5203ac0
rsh-12 <grinvichforum12@mail.ru>
2022-01-17 07:22:49 +0500

    Add findByName method

--- src/main/java/ru/timetable/repository/DayRepository.java
+++ src/main/java/ru/timetable/repository/DayRepository.java
@@ -1,10 +1,14 @@
 package ru.timetable.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.timetable.domain.Day;
+import ru.timetable.domain.Weekday;
 
 @Repository
 public interface DayRepository extends CrudRepository<Day, Integer> {
+
+    Optional<Day> findByName(Weekday name);
 
 }
