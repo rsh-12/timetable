2bf8dcec508069faf5b448bee51fe911d38c629d
rsh-12 <grinvichforum12@mail.ru>
2022-01-18 09:43:46 +0500

    Add AudienceRepositoryTests, findById test method

---
+++ src/test/java/ru/timetable/repository/AudienceRepositoryTests.java
@@ -0,0 +1,45 @@
+package ru.timetable.repository;
+/*
+ * Date: 18.01.2022
+ * Time: 8:38 AM
+ * */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.transaction.annotation.Transactional;
+import ru.timetable.domain.Audience;
+
+@DataJdbcTest
+@Transactional(propagation = NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class AudienceRepositoryTests extends PostgreSqlTestBase {
+
+    @Autowired
+    private AudienceRepository repository;
+
+    private Audience savedEntity;
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+
+        Audience newEntity = new Audience("112");
+        savedEntity = repository.save(newEntity);
+
+        assertEquals(newEntity.getNumber(), savedEntity.getNumber());
+    }
+
+    @Test
+    public void findById() {
+        Audience foundEntity = repository.findById(savedEntity.getId()).orElseThrow();
+        assertEquals(savedEntity, foundEntity);
+    }
+
+}
