78bb3fb68421e5627fb1a54f91f437e3990164a7
rsh-12 <grinvichforum12@mail.ru>
2022-01-21 20:45:24 +0500

    Add AudienceDao, AudienceDaoImpl, add methods

---
+++ src/main/java/ru/timetable/dao/AudienceDao.java
@@ -0,0 +1,16 @@
+package ru.timetable.dao;
+
+import java.util.Optional;
+import ru.timetable.domain.Audience;
+
+public interface AudienceDao {
+
+    Optional<Audience> findById(Integer id);
+
+    int insert(Audience audience);
+
+    void deleteAll();
+
+    Optional<Audience> findByNumber(String number);
+
+}
---
+++ src/main/java/ru/timetable/dao/impl/AudienceDaoImpl.java
@@ -0,0 +1,60 @@
+package ru.timetable.dao.impl;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import ru.timetable.dao.AudienceDao;
+import ru.timetable.dao.util.AudienceRowMapper;
+import ru.timetable.domain.Audience;
+
+@Component
+@Repository
+public class AudienceDaoImpl implements AudienceDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AudienceDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<Audience> findById(Integer id) {
+        String sql = """
+                SELECT * FROM audience
+                WHERE id = ?;
+                """;
+
+        return jdbcTemplate.query(sql, new AudienceRowMapper(), id)
+                .stream().findFirst();
+    }
+
+    @Override
+    public int insert(Audience audience) {
+        String sql = """
+                    INSERT INTO audience(number)
+                    VALUES (?);
+                """;
+
+        return jdbcTemplate.update(sql, audience.getNumber());
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM audience;");
+    }
+
+    @Override
+    public Optional<Audience> findByNumber(String number) {
+        String sql = """
+                SELECT * FROM audience
+                WHERE number = ?;
+                """;
+
+        return jdbcTemplate.query(sql, new AudienceRowMapper(), number)
+                .stream().findFirst();
+    }
+
+}
