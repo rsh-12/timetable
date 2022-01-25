87956fb301a4631aca13ca2e1fd0090ec1be266d
rsh-12 <grinvichforum12@mail.ru>
2022-01-25 08:20:49 +0500

    Update SecurityConfig.java

--- src/main/java/ru/timetable/config/SecurityConfig.java
+++ src/main/java/ru/timetable/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
     }
 
 }
