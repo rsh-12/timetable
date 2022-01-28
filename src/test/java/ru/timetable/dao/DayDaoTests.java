96608e53bc6af57372a87d31c25a5dfae876a0b0
rsh-12 <grinvichforum12@mail.ru>
2022-01-28 06:18:35 +0500

    Add DayDaoTests class, setUp method

---
+++ src/test/java/ru/timetable/dao/DayDaoTests.java
@@ -0,0 +1,42 @@
+package ru.timetable.dao;
+/*
+ * Date: 28.01.2022
+ * Time: 6:11 AM
+ * */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import ru.timetable.PostgreSqlTestBase;
+import ru.timetable.domain.Day;
+import ru.timetable.domain.Weekday;
+
+@SpringBootTest
+@Transactional(propagation = NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class DayDaoTests extends PostgreSqlTestBase {
+
+    @Autowired
+    private DayDao dao;
+
+    private Day savedEntity;
+
+    @BeforeEach
+    void setUp() {
+        dao.deleteAll();
+        assertEquals(0, dao.count());
+
+        dao.insert(new Day(Weekday.MONDAY));
+
+        assertNotNull(savedEntity);
+        assertEquals(1, dao.count());
+    }
+
+}
