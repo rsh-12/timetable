84a80334e12d75624c61241277a9cb87e0145419
rsh-12 <grinvichforum12@mail.ru>
2022-01-28 06:36:14 +0500

    Replace constructor with annotation, update method

--- src/main/java/ru/timetable/dao/impl/DayDaoImpl.java
+++ src/main/java/ru/timetable/dao/impl/DayDaoImpl.java
@@ -5,18 +5,32 @@ package ru.timetable.dao.impl;
  * */
 
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.timetable.dao.DayDao;
+import ru.timetable.dao.util.DayRowMapper;
 import ru.timetable.domain.Day;
 
 @Slf4j
 @Repository
+@RequiredArgsConstructor
 public class DayDaoImpl implements DayDao {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public Optional<Day> findById(Integer id) {
-        return Optional.empty();
+        log.debug("findById: searches for a Day with id={}", id);
+
+        String sql = """
+                SELECT * FROM day
+                WHERE id = ?;
+                """;
+
+        return jdbcTemplate.query(sql, new DayRowMapper(), id).stream()
+                .findFirst();
     }
 
     @Override
