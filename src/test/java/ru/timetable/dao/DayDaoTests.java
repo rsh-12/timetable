7d2cc61cbb13d562a33a6661a039d6c1143545e4
rsh-12 <grinvichforum12@mail.ru>
2022-01-29 09:11:24 +0500

    Add initTable test method

--- src/test/java/ru/timetable/dao/DayDaoTests.java
+++ src/test/java/ru/timetable/dao/DayDaoTests.java
@@ -8,7 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -33,10 +35,31 @@ public class DayDaoTests extends PostgreSqlTestBase {
         dao.deleteAll();
         assertEquals(0, dao.count());
 
-        dao.insert(new Day(Weekday.MONDAY));
+        int result = dao.insert(new Day(Weekday.MONDAY));
+        assertEquals(1, result);
 
+        savedEntity = dao.findByWeekday(Weekday.MONDAY).orElse(null);
         assertNotNull(savedEntity);
-        assertEquals(1, dao.count());
+    }
+
+    @Test
+    public void name() {
+
+    }
+
+    @Test
+    public void initTable() {
+        fillTable();
+
+        int actualQuantity = dao.count();
+        int expectedQuantity = Weekday.values().length;
+
+        assertEquals(expectedQuantity, actualQuantity);
+    }
+
+    public void fillTable() {
+        Arrays.stream(Weekday.values()).map(Day::new)
+                .forEach(day -> dao.insert(day));
     }
 
 }
