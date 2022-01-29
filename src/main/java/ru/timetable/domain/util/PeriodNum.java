b1da8a10c3d28a19d4b5c6c8374700070fab7a38
rsh-12 <grinvichforum12@mail.ru>
2022-01-29 07:57:06 +0500

    Add PeriodNum enum

---
+++ src/main/java/ru/timetable/domain/util/PeriodNum.java
@@ -0,0 +1,21 @@
+package ru.timetable.domain.util;
+
+public enum PeriodNum {
+    FIRST(1),
+    SECOND(2),
+    THIRD(3),
+    FOURTH(4),
+    FIFTH(5),
+    SIXTH(6);
+
+    private final int periodNum;
+
+    PeriodNum(int i) {
+        this.periodNum = i;
+    }
+
+    public int getAsInt() {
+        return periodNum;
+    }
+
+}
