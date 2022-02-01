30c61c3e86fcfabfd2c020f8a913e4e116d99dc5
rsh-12 <grinvichforum12@mail.ru>
2022-02-01 19:39:42 +0500

    Fix PeriodRowMapper

--- src/main/java/ru/timetable/dao/util/PeriodRowMapper.java
+++ src/main/java/ru/timetable/dao/util/PeriodRowMapper.java
@@ -4,8 +4,11 @@ package ru.timetable.dao.util;
  * Time: 6:42 AM
  * */
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.postgresql.util.PGobject;
 import org.springframework.jdbc.core.RowMapper;
 import ru.timetable.domain.Period;
 import ru.timetable.domain.util.Pair;
@@ -13,18 +16,29 @@ import ru.timetable.domain.util.PeriodNum;
 
 public class PeriodRowMapper implements RowMapper<Period> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public Period mapRow(ResultSet rs, int rowNum) throws SQLException {
         int periodNum = rs.getInt("period_num");
-        Pair<String, String> firstHalf = rs.getObject("first_half", Pair.class);
-        Pair<String, String> secondHalf = rs.getObject("second_half", Pair.class);
+        PGobject firstHalfJson = (PGobject) rs.getObject("first_half");
+        PGobject secondHalfJson = (PGobject) rs.getObject("second_half");
 
-        return Period.builder()
-                .id(rs.getInt("id"))
-                .periodNum(PeriodNum.valueOf(periodNum))
-                .firstHalf(firstHalf)
-                .secondHalf(secondHalf)
-                .build();
+        try {
+            Pair firstHalf = objectMapper.readValue(firstHalfJson.getValue(), Pair.class);
+            Pair secondHalf = objectMapper.readValue(secondHalfJson.getValue(), Pair.class);
+
+            return Period.builder()
+                    .id(rs.getInt("id"))
+                    .periodNum(PeriodNum.valueOf(periodNum))
+                    .firstHalf(firstHalf)
+                    .secondHalf(secondHalf)
+                    .build();
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Something went wrong");
+        }
     }
 
 }
