6989c0b7eb0345a77848b35fc7fb8e80af2a038d
rsh-12 <grinvichforum12@mail.ru>
2022-02-05 09:07:35 +0500

    Add findByNameAndSubjectType method

--- src/main/java/ru/timetable/dao/SubjectDao.java
+++ src/main/java/ru/timetable/dao/SubjectDao.java
@@ -1,7 +1,11 @@
 package ru.timetable.dao;
 
+import java.util.Optional;
 import ru.timetable.domain.Subject;
+import ru.timetable.domain.util.SubjectType;
 
 public interface SubjectDao extends CrudDao<Subject> {
+
+    Optional<Subject> findByNameAndSubjectType(String name, SubjectType subjectType);
 
 }
