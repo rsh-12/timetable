175b2229fc0d77cf8a3cea7f38481c64e7f03da7
rsh-12 <grinvichforum12@mail.ru>
2022-02-07 10:11:47 +0500

    Add findByName method

--- src/main/java/ru/timetable/dao/GroupDao.java
+++ src/main/java/ru/timetable/dao/GroupDao.java
@@ -1,7 +1,10 @@
 package ru.timetable.dao;
 
+import java.util.Optional;
 import ru.timetable.domain.Group;
 
 public interface GroupDao extends CrudDao<Group> {
+
+    Optional<Group> findByName(String name);
 
 }
