ce2b2af80b47ae06cff5437bdba08fb847b3181a
rsh-12 <grinvichforum12@mail.ru>
2022-02-07 10:13:58 +0500

    Remove contextLoads test method

--- src/test/java/ru/timetable/TimetableApiApplicationTests.java
+++ src/test/java/ru/timetable/TimetableApiApplicationTests.java
@@ -1,13 +1,9 @@
 package ru.timetable;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TimetableApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
