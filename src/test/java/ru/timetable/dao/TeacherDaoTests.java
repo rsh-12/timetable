77d0231d39790226cd86d585d3020e674c53929a
rsh-12 <grinvichforum12@mail.ru>
2022-02-12 10:04:37 +0500

    Add TeacherDaoTests class

---
+++ src/test/java/ru/timetable/dao/TeacherDaoTests.java
@@ -0,0 +1,45 @@
+package ru.timetable.dao;
+/*
+ * Date: 12.02.2022
+ * Time: 9:49 AM
+ * */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import ru.timetable.PostgreSqlTestBase;
+import ru.timetable.domain.Teacher;
+import ru.timetable.domain.util.Gender;
+
+@SpringBootTest
+@Transactional(propagation = NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class TeacherDaoTests extends PostgreSqlTestBase {
+
+    @Autowired
+    private TeacherDao dao;
+
+    private Teacher savedEntity;
+
+    @BeforeEach
+    void setUp() {
+        dao.deleteAll();
+        assertEquals(0, dao.count());
+
+        Teacher entity = Teacher.builder()
+                .lastName("Волков")
+                .firstName("Егор")
+                .middleName("Михайлович")
+                .gender(Gender.MALE)
+                .build();
+        int result = dao.insert(entity);
+        assertEquals(1, result);
+    }
+
+}
