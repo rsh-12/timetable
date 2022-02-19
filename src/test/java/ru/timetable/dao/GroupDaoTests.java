e5823128db7087219a5962e67ad1d70e502caab1
rsh-12 <grinvichforum12@mail.ru>
2022-02-19 13:04:36 +0500

    Add findAll test method

--- src/test/java/ru/timetable/dao/GroupDaoTests.java
+++ src/test/java/ru/timetable/dao/GroupDaoTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
 
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import ru.timetable.PostgreSqlTestBase;
 import ru.timetable.domain.Group;
@@ -73,6 +76,17 @@ class GroupDaoTests extends PostgreSqlTestBase {
     public void insert_DuplicateKeyException_ShouldReturn0() {
         int result = dao.insert(savedEntity);
         Assumptions.assumeTrue(result == 0);
+    }
+
+    @Test
+    public void findAll() {
+        List.of("ОЗИ-304", "МД-268", "ЗО-325", "ПР-360")
+                .forEach(group -> dao.insert(new Group(group)));
+        assertEquals(5, dao.count());
+
+        Page<Group> page = dao.findAll(PageRequest.of(0, 3));
+        assertEquals(2, page.getTotalPages());
+        assertEquals(3, page.getSize());
     }
 
 }
