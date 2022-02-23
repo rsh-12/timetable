83c463e256285303ac04f069575993a383dc0cf3
rsh-12 <grinvichforum12@mail.ru>
2022-02-23 08:42:06 +0500

    Remove insertAll method

--- src/main/java/ru/timetable/dao/AudienceDao.java
+++ src/main/java/ru/timetable/dao/AudienceDao.java
@@ -1,13 +1,10 @@
 package ru.timetable.dao;
 
-import java.util.List;
 import java.util.Optional;
 import ru.timetable.domain.Audience;
 
 public interface AudienceDao extends CrudDao<Audience> {
 
     Optional<Audience> findByNumber(String number);
-
-    int[][] insertAll(List<Audience> audienceList);
 
 }
