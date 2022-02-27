2b6ee19d8f4306cd4a7917a897654a0b71419c89
rsh-12 <grinvichforum12@mail.ru>
2022-02-27 13:02:38 +0500

    Add some static methods

--- src/main/java/ru/timetable/domain/util/Pair.java
+++ src/main/java/ru/timetable/domain/util/Pair.java
@@ -4,9 +4,22 @@ package ru.timetable.domain.util;
  * Time: 11:30 AM
  * */
 
+import java.time.LocalTime;
+
 /**
  * Represents the time of one half of the lesson, includes the start and end times
  */
 public record Pair(String start, String end) {
+
+    public static Pair of(LocalTime start, LocalTime end) {
+        return new Pair(start.toString(), end.toString());
+    }
+
+    public static Pair of(int hour1, int minute1, int hour2, int minute2) {
+        LocalTime firstHalf = LocalTime.of(hour1, minute1);
+        LocalTime secondHalf = LocalTime.of(hour2, minute2);
+
+        return new Pair(firstHalf.toString(), secondHalf.toString());
+    }
 
 }
